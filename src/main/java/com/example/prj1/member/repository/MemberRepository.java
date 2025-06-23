1b3bfd275d539c04ecb9e0bd63b476ef6e0be322
kk250639 <chesyeo@gmail.com>
2025-06-23 14:12:14 +0900

    회원 목록 보기

--- src/main/java/com/example/prj1/member/repository/MemberRepository.java
+++ src/main/java/com/example/prj1/member/repository/MemberRepository.java
@@ -3,8 +3,11 @@ package com.example.prj1.member.repository;
 import com.example.prj1.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByNickName(String nickName);
+
+    List<Member> findByAll(String email);
 }
--- src/main/java/com/example/prj1/member/service/MemberService.java
+++ src/main/java/com/example/prj1/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.prj1.member.service;
 
 import com.example.prj1.member.dto.MemberForm;
+import com.example.prj1.member.dto.MemberListInfo;
 import com.example.prj1.member.entity.Member;
 import com.example.prj1.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,6 +41,10 @@ public class MemberService {
         } else {
             throw new DuplicateKeyException(data.getId() + "는 이미 있는 아이디입니다.");
         }
+    }
 
+    public List<MemberListInfo> list() {
+
+        return memberRepository.findByAll();
     }
 }
